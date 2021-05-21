087f5d3714006573b03c086fd914852f4ecd6f03
yra3 <https://github.com/yra3>
2021-05-21 22:52:49 +0500

    init

---
+++ main.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+def findedfunk(x):
+    return np.power(x, 2)
+
+
+# def generateregr(count, func, is_linear=False):
+#     x = np.arange(0, count, 1).reshape(count, 1) if is_linear else np.random.randn(count, 1)
+#     y = func(x)
+#     noize = np.random.random(x.shape) * 2 - 1
+#     noize_y = y + noize
+#     xy = np.array([x, noize_y])
+#     c = xy.T
+#     return x, noize_y
+#
+#
+# if __name__ == '__main__':
+#     from network import *
+#     # x, y = generateregr(10, findedfunk, True)
+#     # x = x.reshape(x.shape[0], 1)
+#     # y = y.reshape(x.shape[0], 1)
+#     count = 100
+#     x = np.random.rand(1, count)
+#     # for i in range(count):
+#     #     x[1][i] = 1
+#     y = findedfunk(x[0])
+#
+#     net = Network(
+
+
+def generateregr(count, func, is_linear=False):
+    x = np.arange(0, count, 1).reshape(count, 1) if is_linear else np.random.randn(count, 1)
+    y = func(x)
+    noize = np.random.random(x.shape) * 2 - 1
+    noize_y = y + noize
+    xy = np.array([x, noize_y])
+    c = xy.T
+    return x, noize_y
+
+
+if __name__ == '__main__':
+    from network import *
+    # x, y = generateregr(10, findedfunk, True)
+    # x = x.reshape(x.shape[0], 1)
+    # y = y.reshape(x.shape[0], 1)
+    count = 5
+    x = np.random.rand(1, count)
+    # for i in range(count):
+    #     x[1][i] = 1
+    y = findedfunk(x[0])
+
+    net = Network([1, 10, 1], [sigmoid, x_funk])
+    y_pred = net.backward(x.T, y.reshape(count, 1))
+    loss = np.square(y_pred - y).sum()
+
+
+
+
