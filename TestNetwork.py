40d4eab953031c1a74a8d4265e2fc95a11db03a4
yra3 <https://github.com/yra3>
2021-05-22 16:13:59 +0500

    added count epoch

--- TestNetwork.py
+++ TestNetwork.py
@@ -54,43 +54,39 @@ class TestNetwork:
             a = func(a.dot(w))
         return a
 
-    def backward(self, a, y):
+    def backward(self, a, y, count_epoch):
         au = a
-        for i in range(self.learning_speed):
-            a_without_b_save = []
-            a_with_b_save = []
-            a_with_b_multiplied_w_save = []
-            a_after_funk_save = []
-            # grads_w = [np.zeros(w.shape) for w in self.weights]
-            a = au
-            for w, func in zip(self.weights, self.functions):
-
-                a_without_b_save.append(a)
-                b = np.ones((a.shape[0], 1))
-                a = np.concatenate((a, b), axis=1)
-                a_with_b_save.append(a)
-                a = a.dot(w)
-                a_with_b_multiplied_w_save.append(a)
-                a = func(a)
-                a_after_funk_save.append(a)
-
-            y_pred = a
-            loss = (y_pred - y)
-            y_pred = loss
-            is_first = True
-            # for i in range(1, len(self.sizes)):
-            #     f_p = funk_prime(a_with_b_multiplied_w_save[-i])
-
-            for w, funk_prime, save, a_after_funk, a_with_b_w, j in zip(self.weights[::-1], [nonef_prime][::-1]
-                    , a_with_b_save[::-1], a_after_funk_save[::-1], a_with_b_multiplied_w_save[::-1], range(self.num_layers-1, 0, -1)):
-                z = funk_prime(a_with_b_w)
-                y_pred = y_pred * z
-                if is_first:
-                    is_first = False
-                else:
-                    y_pred = np.delete(y_pred, (y_pred.shape[1]-1), axis=1)
-                delta_w = (save.T.dot(y_pred))
-                self.weights[-j] -= delta_w / float(self.learning_speed)
-                y_pred = y_pred.dot(w.T)
+        for i in range(count_epoch):
+            for i in range(self.learning_speed):
+                a_without_b_save = []
+                a_with_b_save = []
+                a_with_b_multiplied_w_save = []
+                a_after_funk_save = []
+                a = au
+                for w, func in zip(self.weights, self.functions):
+                    a_without_b_save.append(a)
+                    b = np.ones((a.shape[0], 1))
+                    a = np.concatenate((a, b), axis=1)
+                    a_with_b_save.append(a)
+                    a = a.dot(w)
+                    a_with_b_multiplied_w_save.append(a)
+                    a = func(a)
+                    a_after_funk_save.append(a)
+                y_pred = a
+                loss = (y_pred - y)
+                y_pred = loss
+                is_first = True
+                for w, funk_prime, save, a_after_funk, a_with_b_w, j in zip(self.weights[::-1], [nonef_prime][::-1]
+                        , a_with_b_save[::-1], a_after_funk_save[::-1], a_with_b_multiplied_w_save[::-1],
+                                                                            range(self.num_layers - 1, 0, -1)):
+                    z = funk_prime(a_with_b_w)
+                    y_pred = y_pred * z
+                    if is_first:
+                        is_first = False
+                    else:
+                        y_pred = np.delete(y_pred, (y_pred.shape[1] - 1), axis=1)
+                    delta_w = (save.T.dot(y_pred))
+                    self.weights[-j] -= delta_w / float(self.learning_speed)
+                    y_pred = y_pred.dot(w.T)
 
 
